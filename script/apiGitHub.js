e3fe555031616cfb39567146717a733bafc0dc79
dianamartine <diana_martine@outlook.com>
2021-04-29 11:20:25 -0300

    finalized and fix user error

--- script/apiGitHub.js
+++ script/apiGitHub.js
@@ -16,34 +16,27 @@ const getApi = async () => {
   // tratando erro de input
   if (user.value == "") {
     alert("Erro, insira o nome de usuário!");
-  } 
-
-  else {
+  } else {
     // get api github
     const url = `https://api.github.com/users/${user.value}`;
     const data = await fetch(url);
     const userData = await data.json();
-  
+
     if (userData.message == "Not Found") {
       alert("Erro, usuário não encontrado!");
-    }
-    
-    else if (userData.bio == null) {
+    } else if (userData.bio != null) {
       // set visibility container-res and update elements
       containerRes.setAttribute("style", "display: block;");
-      bio.setAttribute("style", "display: none;");
-      img.setAttribute("src", `${userData.avatar_url}`);
-      publicRepos.textContent = `Respositórios: ${userData.public_repos}`;
-      follwers.textContent = `Seguidores: ${userData.followers}`;
-    }
-    
-    else {
-      // update elements
-      bio.setAttribute("style", "display: block;");
       bio.textContent = userData.bio;
       img.setAttribute("src", `${userData.avatar_url}`);
       publicRepos.textContent = `Respositórios: ${userData.public_repos}`;
       follwers.textContent = `Seguidores: ${userData.followers}`;
-    }
+    } else if (userData.bio == null) {
+      // update elements
+       bio.setAttribute("style", "display: none;");
+       img.setAttribute("src", `${userData.avatar_url}`);
+       publicRepos.textContent = `Respositórios: ${userData.public_repos}`;
+       follwers.textContent = `Seguidores: ${userData.followers}`;
+     }
   }
 };
